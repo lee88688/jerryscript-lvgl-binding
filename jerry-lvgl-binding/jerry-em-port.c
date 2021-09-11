f24cd0fc8a847f7fa31717239a9809ed4adff4dc
lee88688 <lee88688@163.com>
2021-09-11 16:04:47 +0800

    feat: add websocket debugger port

--- jerry-lvgl-binding/jerry-em-port.c
+++ jerry-lvgl-binding/jerry-em-port.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdarg.h>
 #include <time.h>
 #include <sys/time.h>
 #include "emscripten/emscripten.h"
+#include "emscripten/websocket.h"
 #include "jerryscript-port.h"
 #include "jerryscript-ext/debugger.h"
 
@@ -73,4 +75,79 @@ jerry_context_t *jerry_port_get_current_context ()
 // sleep
 void jerry_port_sleep (uint32_t sleep_time) {
   emscripten_sleep(sleep_time);
+}
+
+// ----- debugger ------
+typedef struct
+{
+  jerry_debugger_transport_header_t header; /**< transport header */
+  EMSCRIPTEN_WEBSOCKET_T websocket;
+} jerry_em_debugger_transport_t;
+
+typedef struct debugger_received_data {
+  uint32_t length;
+  uint8_t *data;
+} debugger_received_data;
+
+static debugger_received_data received_data = {
+  .length = 0,
+  .data = NULL
+};
+
+static const EmscriptenWebSocketCreateAttributes attr = {
+  .url = "ws://127.0.0.1:9090",
+  .protocols = NULL,
+  .createOnMainThread = 0
+};
+
+static EMSCRIPTEN_WEBSOCKET_T websocket = -1;
+static bool is_open = false;
+
+EM_BOOL em_websocket_onopen(int eventType, const EmscriptenWebSocketOpenEvent *websocketEvent, void *userData) {
+  printf("websocket opened!\n");
+  is_open = true;
+}
+
+EM_BOOL em_websocket_message(int eventType, const EmscriptenWebSocketMessageEvent *websocketEvent, void *userData) {
+  received_data.data = realloc(received_data.data, received_data.length + websocketEvent.numBytes);
+  memcpy(received_data.data + received_data.length, websocketEvent.data, websocketEvent.numBytes);
+}
+
+static bool jerry_em_debugger_send (jerry_debugger_transport_header_t *header, uint8_t *message, size_t len) {
+  printf("send %d bytes.\n", len);
+  EMSCRIPTEN_RESULT result = emscripten_websocket_send_binary(((jerry_em_debugger_transport_t *)header)->websocket, message, len);
+  return result > 0;
+}
+
+static bool jerry_em_debugger_receive (jerry_debugger_transport_header_t *header, jerry_debugger_transport_receive_context_t *context) {
+  printf("receive %d bytes.\n", context->received_length);
+}
+
+static void jerry_em_debugger_close (jerry_debugger_transport_header_t *header) {
+  printf("close!\n");
+}
+
+bool jerry_em_debugger_create() {
+  EMSCRIPTEN_WEBSOCKET_T socket = emscripten_websocket_new(&attr);
+  if (socket < 0) {
+    return false;
+  }
+  emscripten_websocket_set_onopen_callback(socket, NULL, em_websocket_onopen);
+  emscripten_websocket_set_onmessage_callback(socket, NULL, em_websocket_message);
+  for (int i = 0; i < 10; i++) {
+    if (is_open) {
+      break;
+    }
+    emscripten_sleep(10);
+  }
+  
+  jerry_debugger_transport_header_t *header = jerry_heap_alloc(sizeof(jerry_em_debugger_transport_t));
+  if (!header) return false;
+
+  header->send = jerry_em_debugger_send;
+  header->receive = jerry_em_debugger_receive;
+  header_p->close = jerry_em_debugger_close;
+
+  jerry_debugger_transport_add(header, 0, JERRY_DEBUGGER_TRANSPORT_MAX_BUFFER_SIZE, 0, JERRY_DEBUGGER_TRANSPORT_MAX_BUFFER_SIZE);
+  return true;
 }
