872785ab048188679fa29436b4691c660c1bc0d7
lee <lee88688@163.com>
2021-09-29 17:57:35 +0800

    feat: createLvglStyles global api, add optObjStyles function

--- jerry-lvgl-binding/lvgl-obj.c
+++ jerry-lvgl-binding/lvgl-obj.c
@@ -28,15 +28,64 @@ jerry_value_t lvgl_obj_constructor(const jerry_call_info_t *info, const jerry_va
     return jerry_create_undefined();
 }
 
-/**
- * free_cb在gc的时候有没有调用
- */
+jerry_value_t lvgl_obj_opt_styles(const jerry_call_info_t *info, const jerry_value_t argv[], const jerry_length_t argc) {
+    if (argc < 2) return jerry_create_undefined();
+
+    // style obj can be array or single obj
+    // argv[0] is type 0 for delete and 1 for add, argv[1] for styles
+    lv_obj_t *obj = NULL;
+    js_lvgl_get_native_info(info->this_value, &obj);
+
+    uint32_t type = jerry_value_as_uint32(argv[0]);
+    jerry_value_t obj_or_arr = argv[1];
+    bool is_array = jerry_value_is_array(argv[1]);
+    if (!is_array) {
+        lv_style_t *style = NULL;
+        bool have_native_style = jerry_get_object_native_pointer(obj_or_arr, &style, &lvgl_obj_native_info);
+        if (have_native_style) {
+            jerry_value_t selector = jerryx_get_property_str(obj_or_arr, "selector");
+            if (type == 1) {
+                lv_obj_add_style(obj, style, jerry_value_as_uint32(selector));
+            } else {
+                lv_obj_remove_style(obj, style, jerry_value_as_uint32(selector));
+            }
+            jerry_release_value(selector);
+        }
+    } else {
+        jerry_value_t arr = obj_or_arr;
+        uint32_t len = jerry_get_array_length(arr);
+        uint32_t i = 0;
+        for(i = 0; i < len; i++) {
+            jerry_value_t v = jerry_get_property_by_index(arr, i);
+            lv_style_t *style = NULL;
+            bool have_native_style = jerry_get_object_native_pointer(v, &style, &lvgl_obj_native_info);
+            if (have_native_style) {
+                jerry_value_t selector = jerryx_get_property_str(v, "selector");
+                if (type == 1) {
+                    lv_obj_add_style(obj, style, jerry_value_as_uint32(selector));
+                } else {
+                    lv_obj_remove_style(obj, style, jerry_value_as_uint32(selector));
+                }
+                jerry_release_value(selector);
+            }
+
+            jerry_release_value(v);
+        }
+    }
+
+    return jerry_create_undefined();
+}
+
 void js_lvgl_obj_init() {
     jerry_value_t global = jerry_get_global_object();
 
     jerry_value_t constructor = jerry_create_external_function(lvgl_obj_constructor);
     jerry_value_t proto = jerry_create_object();
     jerry_set_prop_list(proto, js_obj_prototype_methods, countof(js_obj_prototype_methods));
+
+    jerry_value_t set_obj_styles = jerry_create_external_function(lvgl_obj_opt_styles);
+    jerryx_set_property_str(proto, "optObjStyles", set_obj_styles);
+    jerry_release_value(set_obj_styles);
 
     jerry_value_t native_info = jerry_create_number((uint32_t) &lvgl_obj_native_info);
     jerryx_set_property_str(proto, NATIVE_INFO_PROP_STR, native_info);
---
+++ jerry-lvgl-binding/lvgl-style.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "lvgl.h"
+#include "jerryscript.h"
+#include "jerryscript-ext/handler.h"
+
+
+static void lvgl_style_free_cb (void *native_p, jerry_object_native_info_t *info_p) {
+    lv_style_t *style = (lv_obj_t *) native_p;
+    // todo: prop_cnt == 1
+    if (style->prop_cnt > 0) {
+        free(style->v_p.const_props);
+    }
+    free(style);
+}
+
+static const jerry_object_native_info_t lvgl_style_native_info = {
+    .free_cb = lvgl_style_free_cb,
+};
+
+static bool create_lvgl_style_foreach(const jerry_value_t obj_prop_name, const jerry_value_t obj_prop_value, void* user_data) {
+    jerry_value_t* argv = (jerry_value_t *) user_data;
+    jerry_value_t obj = argv[0];
+
+    lv_style_t *style = (lv_style_t *) malloc(sizeof(lv_style_t));
+    lv_style_init(style);
+    style->is_const = 1; // prevent lvgl modifying
+    jerry_value_t attributes = jerryx_get_property_str(obj_prop_value, "attributes");
+    uint8_t prop_cnt = jerry_get_array_length(attributes);
+    // todo: prop_cnt == 1
+    if (prop_cnt != 0) {
+        style->prop_cnt = prop_cnt;
+        lv_style_const_prop_t *style_props = malloc(sizeof(lv_style_const_prop_t) * prop_cnt);
+        uint8_t i = 0;
+        for (i = 0; i < prop_cnt; i++) {
+            jerry_value_t attr = jerry_get_property_by_index(attributes, i);
+            jerry_value_t name = jerryx_get_property_str(attr, "name");
+            jerry_value_t value = jerryx_get_property_str(attr, "value");
+
+            lv_style_prop_t prop_name = jerry_value_as_uint32(name);
+            style_props[i].prop = prop_name;
+            switch (prop_name) {
+                // color
+                case LV_STYLE_TEXT_COLOR:
+                case LV_STYLE_BG_COLOR:
+                    uint32_t v = jerry_value_as_uint32(value);
+                    lv_style_value_t style_value = { .color = LV_COLOR_MAKE((v >> 16) & 0xff, (v >> 8) & 0xff, v & 0xff) };
+                    style_props[i].value = style_value;
+                    break;
+                
+                // default for most of props
+                default:
+                    style_props[i].value.num = jerry_value_as_uint32(value);
+                    break;
+            }
+
+            jerry_release_value(value);
+            jerry_release_value(name);
+            jerry_release_value(attr);
+        }
+        style->v_p.const_props = style_props;
+    }
+
+    jerry_set_object_native_pointer(
+        obj_prop_value,
+        style,
+        &lvgl_style_native_info
+    );
+
+    jerry_release_value(attributes);
+    return true;
+}
+
+static jerry_value_t create_lvgl_style(const jerry_call_info_t *info, const jerry_value_t argv[], const jerry_length_t argc) {
+    if (argc == 0) return jerry_create_undefined();
+
+    // jerry_value_t js_style_obj = argv[0];
+    bool iteration_result = jerry_foreach_object_property(argv[0], create_lvgl_style_foreach, argv);
+
+    return jerry_create_undefined();
+}
+
+void js_lvgl_style_init() {
+    jerry_value_t global = jerry_get_global_object();
+
+    jerry_value_t func = jerry_create_external_function(create_lvgl_style);
+    jerryx_set_property_str(global, "createLvglStyles", func);
+
+    jerry_release_value(global);
+}
